f6c0b2830f685d162286d7f7ae09aa6924f45c42
raosan <raosanf.lillahi@gmail.com>
2021-05-08 15:10:02 +0700

    return json

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,5 +1,6 @@
+import json
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify, Response
 )
 from werkzeug.exceptions import abort
 
@@ -12,11 +13,19 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
+        'SELECT id, title, body'
+        ' FROM post'
     ).fetchall()
-    return render_template('blog/index.html', posts=posts)
+
+    data = []
+    columns = ['id', 'title', 'body', 'author_id', 'username']
+
+    for post in posts:
+        data.append(dict(zip(columns, post)))
+  
+    return Response(json.dumps(data),  mimetype='application/json')
+    # return render_template('blog/index.html', posts=posts)
+
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
